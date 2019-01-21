d4b0ca9c2c32a9653e1c542826b0646eac5f7b1e
Howard <hliu1461603525@gmail.com>
2019-01-21 02:24:23 -0500

    Init commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
